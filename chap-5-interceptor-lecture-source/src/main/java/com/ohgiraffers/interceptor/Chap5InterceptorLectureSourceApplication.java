5c4980ee330d3856408181d9e46432960ef60551
JuneYoungDo <ehwnsdud1004@naver.com>
2025-03-20 17:33:04 +0900

    Practice for Class

---
+++ chap-5-interceptor-lecture-source/src/main/java/com/ohgiraffers/interceptor/Chap5InterceptorLectureSourceApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.interceptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap5InterceptorLectureSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap5InterceptorLectureSourceApplication.class, args);
+    }
+
+}
